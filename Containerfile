18587c1295a56ff56a299a38a7917ec755b643af
cwilkers <cwilkers@redhat.com>
2023-11-06 07:57:56 -0600

    Updated with recursion

--- Containerfile
+++ Containerfile
@@ -1,11 +1,7 @@
 FROM golang
 WORKDIR /app
 
-COPY go.mod ./
-RUN go mod download
-
-COPY *.go *.html ./
-COPY words /words
+COPY go.mod words *.html *.go ./
 
 RUN CGO_ENABLED=0 GOOS=linux go build -o /jumble
 
--- main.go
+++ main.go
@@ -2,17 +2,23 @@ package main
 
 import (
 	"bufio"
+	"cmp"
+	"encoding/json"
 	"fmt"
 	"html/template"
 	"log"
 	"net/http"
 	"os"
-	"sort"
+	"slices"
 	"strings"
 )
 
 type Data struct {
 	Answer []string
+}
+
+type Health struct {
+	Words int
 }
 
 // Vars that need to be global
@@ -21,7 +27,7 @@ var wordHash map[string][]string
 // Hash function that alphabetizes letters in a word
 func hash(s string) string {
 	a := strings.Split(s, "")
-	sort.Strings(a)
+	slices.Sort(a)
 	return strings.Join(a, "")
 }
 
@@ -46,12 +52,61 @@ func readWords(wordFile string) map[string][]string {
 	return wordHash
 }
 
+func deDupe(a []string) []string {
+	existList := make(map[string]bool)
+	list := []string{}
+	for _, i := range a {
+		if _, v := existList[i]; !v {
+			existList[i] = true
+			list = append(list, i)
+		}
+	}
+	comparator := func(a, b string) int {
+		c := cmp.Compare(len(b), len(a))
+		if c == 0 {
+			return cmp.Compare(a, b)
+		} else {
+		    return c
+		}
+	}
+	slices.SortFunc(list, comparator)
+	return list
+}
+
+func findAnswers(query string) []string {
+	answer := wordHash[query]
+	l := len(query)
+	if l > 3 {
+		a := strings.Split(query, "")
+		answer = append(answer, findAnswers(strings.Join(a[1:l], ""))...)
+		for i := 1; i < l-1; i++ {
+			b := make([]string, l-1)
+			copy(b, a[0:i])
+			b = append(b, a[i+1:l]...)
+			answer = append(answer, findAnswers(strings.Join(b, ""))...)
+		}
+		answer = append(answer, findAnswers(strings.Join(a[0:l-1], ""))...)
+	}
+	return answer
+}
+
 func formHandler(w http.ResponseWriter, r *http.Request) {
-    query := r.FormValue("search")
-	answer:= wordHash[hash(query)]
-	d := &Data{Answer: answer}
+	query := r.FormValue("search")
 	t, _ := template.ParseFiles("index.html")
-	t.Execute(w, d)
+	t.Execute(w, &Data{Answer: deDupe(findAnswers(hash(query)))})
+}
+
+func healthHandler(w http.ResponseWriter, r *http.Request) {
+    health := Health{
+		Words: len(wordHash),
+	}
+	j,_ := json.MarshalIndent(health, "", "  ")
+	if health.Words > 0 {
+		w.WriteHeader(http.StatusOK)
+	} else {
+		w.WriteHeader(http.StatusInternalServerError)
+	}
+	fmt.Fprint(w, string(j))
 }
 
 // Set up a webserver
@@ -64,10 +119,10 @@ func main() {
 	address := fmt.Sprintf("%s:%s", "0.0.0.0", httpPort)
 	wordFile := os.Getenv("WORDFILE")
 	if wordFile == "" {
-		wordFile = "/words"
+		wordFile = "words"
 	}
 	wordHash = readWords(wordFile)
-	fmt.Printf("Read %v words\n", len(wordHash))
-    http.HandleFunc("/", formHandler)
+	http.HandleFunc("/", formHandler)
+	http.HandleFunc("/health", healthHandler)
 	log.Fatal(http.ListenAndServe(address, nil))
 }
